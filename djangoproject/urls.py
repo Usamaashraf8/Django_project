d1c90288ccf7651b2f9da6759584907abe6c8965
usamaashraf785 <usamachoudhary785@gmail.com>
2023-09-04 21:46:39 +0200

    added index.html in home app

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Home.views import *
+from home.views import *
 urlpatterns = [
 
     path('', home , name='home'),
--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hey i am django server!!")
+    return render(request, "home/index.html")
 
 def success_page(request):
     return HttpResponse("Hey this is sucess_page!")
